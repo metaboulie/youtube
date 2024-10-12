6921890a00a5981a8744fa41cfd715cd350c881d
metaboulie <eugeneheiner14@gmail.com>
2024-10-12 21:32:24 +0800

    add section intro

--- deepboredom-intro/src/routes/+page.svelte
+++ deepboredom-intro/src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-import VideoIntro from './VideoIntro.svelte';
+// import VideoIntro from './VideoIntro.svelte';
+import SectionIntro from './SectionIntro.svelte';
 
 </script>
 
 <main class="bg-nord-bg-warp min-h-screen">
-    <VideoIntro videoTitle="Unravel The Dream Paper Reader" />
+    <!-- <VideoIntro videoTitle="Unravel The Dream Paper Reader" /> -->
+    <SectionIntro sectionTitle="The Dream Paper Reader" sectionDescription="neovim + docling + ell + ollama" />
 </main>
